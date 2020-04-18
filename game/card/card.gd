5bfdb4f2f8cb3b2ad48c9409c2615a21c2c1a0a7
Justin Wash <justin.wash@pm.me>
2020-04-17 22:50:17 -0500

    draggable cards

--- game/card/card.gd
+++ game/card/card.gd
@@ -1,4 +1,15 @@
-extends Node2D
+extends KinematicBody2D
 
-func _ready():
-	pass
+# Pickable needs to be selected from the inspector
+
+var can_grab = false
+var grabbed_offset = Vector2()
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		can_grab = event.pressed
+		grabbed_offset = position - get_global_mouse_position()
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_grab:
+		position = get_global_mouse_position() + grabbed_offset
